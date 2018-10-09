d99465fb70db74377ab3f5c6a4b1cffec1b6af48
Garrett Shirley <garrettjshirl@hotmail.com>
2018-10-09 09:50:27 +0200

    Initial file/program made

---
+++ tpudpserver/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+void timeProtocolUDPServer();
+
+int main(){
+    cout << "Hello world" << endl;
+
+    return 0;
+}
+
+void timeProtocolUDPServer(){
+
+}
