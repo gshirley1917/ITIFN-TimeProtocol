c8f81ecbc7295718a815933d0886566dd468e836
Garrett Shirley <garrettjshirl@hotmail.com>
2018-10-09 09:19:11 +0200

    Program made

--- tpudpclient/main.cpp
+++ tpudpclient/main.cpp
@@ -24,4 +24,30 @@ int main(int argc, const char * argv[]){
 void timeProtocolUDPClient(const char address[]){
     int port = 123;
     int maxLength = 1024;
+    unsigned char msg[48]={010,0,0,0,0,0,0,0,0};
+    unsigned long buffer[maxLength];
+    struct protoent * proto;
+    struct sockaddr_in server_addr;
+    int socket;
+    long timeLimit;
+
+    proto = getprotobyname("udp");
+    socket = socket(PF_INET, SOCK_DGRAM, proto->p_proto);
+    
+    memset( &server_addr, 0, sizeof( server_addr ));
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(address);
+    server_addr.sin_port=htons(port);
+
+    int i = i=sendto(socket, msg, sizeof(msg), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    struct sockaddr socketAddr;
+    socklen_t socketAddr_l = sizeof(socketAddr);
+    i = recvfrom(socket, buffer, 48, 0, &socketAddr,&socketAddr_l);
+
+    timeLimit = ntohl((time_t)buffer[4]); //# get transmit time
+    timeLimit-= 2208988800U;
+    cout << "NTP time is " << ctime(&timeLimit) << endl;
+    i=time(0);
+    cout << "System time is " << (i-tmit) << " seconds off" << endl;
+ 
 }
