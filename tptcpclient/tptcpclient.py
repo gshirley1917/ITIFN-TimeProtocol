8147c3a12265820a099e6e104690b03e899575ea
cuong <cuongcao911@gmail.com>
2018-10-10 18:48:26 +0200

    tcp working properly

--- tptcpclient/tptcpclient.py
+++ tptcpclient/tptcpclient.py
@@ -1,8 +1,12 @@
 import socket
+import datetime, time
+
 mysocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(("127.0.0.1", 37))
 data = mysocket.recv(2048)
+data= int.from_bytes(data, byteorder="big")
 
 print (data)
+
 mysocket.send(b"Connected to the client")
 mysocket.close()
--- tptcpserver/tptcpserver.py
+++ tptcpserver/tptcpserver.py
@@ -1,22 +1,21 @@
 import socket
 import datetime, time
-import struct
 
 dts = datetime.datetime.utcnow()
-epochtime = round(time.mktime(dts.timetuple()) + dts.microsecond/1e6)
+epochtime1 = round(time.mktime(dts.timetuple()) + dts.microsecond/1e6)
+epochtime= epochtime1 + 2208988800
 
+print("Server started")
+print("Listening")
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.bind(("127.0.0.1", 37))
 mysocket.listen(5)
-(client, (ip,port))=mysocket.accept()
-thetimenow = time.time()
-ba = bytearray(struct.pack("f", thetimenow))  
+(client, (ip,port))=mysocket.accept() 
+time=(epochtime).to_bytes(4, byteorder='big')
 
-print(thetimenow)
-#print ("To Local Time: ", time.ctime(thetimenow))
-
-client.send(ba)
+client.send(time)
 data = client.recv(2048)
 print (data)
 mysocket.close()
+
